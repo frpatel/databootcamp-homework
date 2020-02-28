f22c8709774241437bf8787cdf131d99eb9e4e8b
Falgun Patel <falgunrpatel@hotmail.com>
2020-02-28 14:06:56 -0500

    updated changes moved

--- javascript-challenge/UFO-level-2/static/js/app.js
+++ javascript-challenge/UFO-level-2/static/js/app.js
@@ -1,58 +1,72 @@
 // from data.js
 var tableData = data;
-console.log(tableData);
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// UFO Sighting values for each column
-tableData.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
-    // Append one table row `tr` for each UFO Sighting object
-    var row = tbody.append("tr");
+// Display the entire dataset as default
 
-    // Use `Object.entries` to console.log each UFO Sighting value
-    Object.entries(ufoSighting).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
+tableData.forEach((report) => {
+    console.log(report);
+    var row = tbody.append('tr');
 
-
-// Select the button
-var button = d3.select("#filter-btn");
-button.on("click", function() {
-
-    tbody.html("");
-
-    // Select the input date, state, shape and get the raw HTML nodes
-    var inputElement = d3.select("#input");
-    // Get the value property of the input date, state, shape
-    var inputValue = inputElement.property("value");
-    // console.log input value
-    // console.log(inputValue);
-    // Filter Data with datetime equal to input value
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue ||
-                                                    sighting.city === inputValue ||
-                                                    sighting.state === inputValue ||
-                                                    sighting.country === inputValue ||
-                                                    sighting.shape === inputValue);
-    // console.log filter values
-    console.log(filteredData);
-
-
-    filteredData.forEach(function(selections) {
-
-    console.log(selections);
-    // Append one table row `tr` for each UFO Sighting object
-    var row = tbody.append("tr");
-    // Use `Object.entries` to console.log each UFO Sighting value
-    Object.entries(selections).forEach(function([key, value]) {
+    Object.entries(report).forEach(([key, value]) => {
         console.log(key, value);
-        // Append a cell to the row for each value
-        var cell = row.append("td");
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
+// Select the submit button in the html file
+var clickHandler = d3.select("#filter-btn");
+
+//Click event
+clickHandler.on("click", function() {
+
+//Remove existing table
+d3.select("tbody").html("");
+
+//Prevent page from refreshing
+d3.event.preventDefault();
+
+// Get the value property of the input elements and set all text to lowercase
+var dateTime = d3.select("#datetime").property("value");
+
+var selectedCountry = d3.select("#country").property("value").toLowerCase();
+
+var selectedState = d3.select("#state").property("value").toLowerCase();
+
+var selectedCity = d3.select("#city").property("value").toLowerCase();
+
+var selectedShape = d3.select("#shape").property("value").toLowerCase();
+
+// initialize tableData as filteredData
+filteredData = tableData;
+
+if (dateTime) {
+    filteredData = filteredData.filter(record => record.datetime === dateTime);
+}
+if (selectedCountry) {
+    filteredData = filteredData.filter(record => record.country === selectedCountry);
+}
+if (selectedState) {
+    filteredData = filteredData.filter(record => record.state === selectedState);
+}
+if (selectedCity) {
+    filteredData = filteredData.filter(record => record.city === selectedCity);
+}
+if (selectedShape) {
+    filteredData = filteredData.filter(record => record.shape === selectedShape);
+}
+
+// Display the filtered dataset
+
+filteredData.forEach((report) => {
+    var row = tbody.append('tr');
+
+    Object.entries(report).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append('td');
         cell.text(value);
     });
 });
